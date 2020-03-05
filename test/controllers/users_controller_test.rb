905d173e6aabf367bbc83f1362f49e4a35029309
DenisRem <ihor.moskvun5@gmail.com>
2020-03-05 14:59:17 +0200

    Finish user edit, update, index, and destroy actions

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -12,6 +12,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
 
+
   test "should get new" do
     get signup_path
     assert_response :success
